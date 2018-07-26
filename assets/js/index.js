e806191f20f77883dc014c1df741d487a1c6e738
lu97chi <lu97is@gmail.com>
2018-07-26 14:11:41 -0500

    v1.10

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
-	$(function(){
+$(document).ready(function () {
+
 	var navbar = $('.navbar');
 	$('.show-menu-bot').css('display', 'none')
 	$(window).scroll(function () {
@@ -9,29 +10,29 @@
 		}
 	});
 
-	$('.product-items-container').on('click', '.option-list-us', function ()  {
+	$('.product-items-container').on('click', '.option-list-us', function () {
 		let i = $(this);
 		i.parent().siblings().removeClass('active-item');
-		i.parent().hasClass('active-item')? i.parent().removeClass('active-item'):i.parent().addClass('active-item')
-		$('.us-dynamic').collapse('hide')	
+		i.parent().hasClass('active-item') ? i.parent().removeClass('active-item') : i.parent().addClass('active-item')
+		$('.us-dynamic').collapse('hide')
 	});
 
-	$('.product-items-container').on('click', '.option-list-products', function(){
+	$('.product-items-container').on('click', '.option-list-products', function () {
 		let i = $(this);
 		i.parent().siblings().removeClass('active-item');
-		i.parent().hasClass('active-item')? i.parent().removeClass('active-item'):i.parent().addClass('active-item')
+		i.parent().hasClass('active-item') ? i.parent().removeClass('active-item') : i.parent().addClass('active-item')
 
-		$('.products-dymamic').collapse('hide')	
+		$('.products-dymamic').collapse('hide')
 	})
-	$('.product-items-container').on('click', '.option-list-culture', function(){
+	$('.product-items-container').on('click', '.option-list-culture', function () {
 		let i = $(this);
 		i.parent().siblings().removeClass('active-item');
-		i.parent().hasClass('active-item')? i.parent().removeClass('active-item'):i.parent().addClass('active-item')
+		i.parent().hasClass('active-item') ? i.parent().removeClass('active-item') : i.parent().addClass('active-item')
 
-		$('.culture-dynamic').collapse('hide')	
+		$('.culture-dynamic').collapse('hide')
 	})
 
-	
+
 
 
 	let menu = false;
@@ -46,25 +47,56 @@
 				display: 'none'
 			});
 	})
-	$('.navigation').on('click', (e)=>{
+	$('.navigation').on('click', (e) => {
 		let navigate = e.target.className.split(' ')[1];
 		let dina;
-		$(window).width() > 768? dina = $('#nav-d').outerHeight() : dina = Math.floor($('#nav-m').outerHeight());
+		$(window).width() > 768 ? dina = $('#nav-d').outerHeight() : dina = Math.floor($('#nav-m').outerHeight());
 		console.log(dina)
-		
+
 		$('.show-menu-bot').animate({
 			height: 'toggle',
 			display: 'none'
-		},500, function(){
+		}, 500, function () {
 			$('html, body').animate({
 				scrollTop: $(`#${navigate}`).offset().top - dina
-			},500)
+			}, 500)
 		})
 	})
 
-	$('.contact-button').on('click', ()=>{
+	$('.contact-button').on('click', () => {
 		$('html, body').animate({
 			scrollTop: $(`#contact-section`).offset().top - 100
-		},500)
+		}, 500)
 	})
+	// despues averiguo que hace :D
+	$("#carouselIndicators2").on("touchstart", function (event) {
+		var xClick = event.originalEvent.touches[0].pageX;
+		$(this).one("touchmove", function (event) {
+			var xMove = event.originalEvent.touches[0].pageX;
+			if (Math.floor(xClick - xMove) > 5) {
+				$(this).carousel('next');
+			}
+			else if (Math.floor(xClick - xMove) < -5) {
+				$(this).carousel('prev');
+			}
+		});
+		$("#carouselIndicators2").on("touchend", function () {
+			$(this).off("touchmove");
+		});
+	});
+
+	// console.log($('#us-text-left').height())
+	// console.log($('.us-selected').height())
+	$('.product-items-container').on('click', ()=>{
+		$('.us-selected').css("height", $("#us-text-left").height())
+		$('.products-selected').css("height", $("#products-text-left").height())
+	$('.culture-selected').css("height", $("#culture-text-left").height())
+
+	})
+
 })
+
+
+
+
+
